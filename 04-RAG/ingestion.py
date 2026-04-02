b5d636ec0f2ca58b8bf6adab9b70e44cbfa09cb7
Raul Morales <irmoralesb@hotmail.com>
2026-04-01 23:06:12 -0600

    RAG

--- 04-RAG/ingestion.py
+++ 04-RAG/ingestion.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from pathlib import Path
 from langchain_community.document_loaders import TextLoader
@@ -5,13 +6,33 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
-
-ROOT = Path(__file__).resolve().parent.parent
+CURRENT_DIRECTORY = Path(__file__).resolve().parent
+ROOT = CURRENT_DIRECTORY.parent
 load_dotenv(ROOT / ".env")
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+INDEX_NAME = os.environ.get("INDEX_NAME")
 
 
 def main():
-    pass
+    print("Ingesting...")
+    loader = TextLoader(Path.joinpath(CURRENT_DIRECTORY,
+                        "mediumblog1.txt"), encoding="UTF-8")
+    document = loader.load()
+
+    print("Splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks.")
+
+    embeddings = OpenAIEmbeddings(openai_api_type=OPENAI_API_KEY)
+
+    print("Ingesting...")
+
+    PineconeVectorStore.from_documents(
+        texts, embeddings, index_name=INDEX_NAME)
+
+    print("Finish!")
 
 
 if __name__ == "__main__":
